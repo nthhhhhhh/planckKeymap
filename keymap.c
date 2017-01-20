7269580d936648b10e6a51e60457de30b79845d8
joseph <j.sph@me.com>
2017-01-20 14:18:12 -0600

    New macros

--- keymap.c
+++ keymap.c
@@ -42,50 +42,79 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | FN0  | Ctrl | Alt  | Lower|  GUI |    Space    | Entr | Raiz | GUI  | Lead | Alt  |
+ * | FWD  | Ctrl | Alt  | Lower|  GUI |    SPACE    | Entr | Raiz | GUI  | Lead | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
 [0] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SCLN), KC_QUOT},
   {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  TD(TD_SLSH), KC_RSPC},
-  {KC_FN0,  KC_LCTL, KC_LALT, M(1), KC_LGUI,    KC_SPC,  KC_SPC,  KC_ENT,  M(2),    KC_RGUI, KC_LEAD, KC_RALT}
+  {M(3),  KC_LCTL, KC_LALT, M(1), KC_LGUI,    KC_SPC,  KC_SPC,  KC_ENT,  M(2),    KC_RGUI, KC_LEAD, KC_RALT}
 },
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |      |      | Caps |      |      |      |  KP7 |  KP8 |  KP9 |  KP- | Bksp |
+ * | Tab  |      |  DOT | Caps |      |      |      |  KP7 |  KP8 |  KP9 |  KP- | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | MTCE |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  KP4 |  KP5 |  KP6 |  KP+ |  /   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |  KP1 |  KP2 |  KP3 |  KP* |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | FN0  | Ctrl |  Alt |  GUI |      |             |  KP0 | LEFT | DOWN |  UP  | RIGHT|
+ * | FN0  | Ctrl |  Alt | XXXX |  GUI |    SPACE    |  KP0 | LEFT | DOWN |  UP  | RIGHT|
  * `-----------------------------------------------------------------------------------'
  */
 [1] = {
-  {KC_TAB,  _______, _______,  KC_CAPS, _______,  _______, _______, KC_7, KC_8, KC_9, KC_PMNS, KC_BSPC},
+  {KC_TAB,  _______, KC_CAPS,  KC_DOT, _______,  _______, _______, KC_7, KC_8, KC_9, KC_PMNS, KC_BSPC},
   {KC_DEL,  KC_F1,   KC_F2,    KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_4, KC_5, KC_6, KC_PPLS, KC_SLSH},
   {_______, KC_F7,   KC_F8,    KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_1, KC_2, KC_3, KC_PAST, KC_ENT },
-  {KC_FN0,  KC_LCTL, KC_LALT,  KC_LGUI, _______, _______, KC_DOT,  KC_0, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
+  {KC_FN0,  KC_LCTL, KC_LALT,  KC_TRNS, KC_LGUI, KC_SPACE,  _______, KC_0, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
 },
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | MTCE |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |      |      |  \   |
+ * | MTCE | BWORD| FWORD| HOME |  END |      |      |   -  |   =  |DLWORD|DRWORD|  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   [  |   ]  |      |      | Shift|
+ * | Shift| PREV | PLAY | NEXT | MUTE | Vol- | Vol+ |   [  |   ]  |      |      | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | RESET|      |      |      |      |             | Enter|      |      |      |      |
+ * | RESET|      |      |      |      |             | Enter| XXXX |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [2] = {
   {KC_TILD,  KC_EXLM,  KC_AT,  KC_HASH,    KC_DLR,    KC_PERC,   KC_CIRC,  KC_AMPR,   KC_ASTR,  KC_LPRN,   KC_RPRN,    KC_BSPC},
-  {CTL_T(KC_ESC),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  _______, _______, KC_BSLS},
-  {KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TD(TD_LBRACKET), TD(TD_RBRACKET), _______, _______, KC_RSFT},
-  {RESET,   _______, _______, _______, _______, _______, _______, KC_ENT, _______, _______, _______, _______}
+  {CTL_T(KC_ESC),  LALT(KC_LEFT), LALT(KC_RIGHT), RGUI(KC_LEFT), RGUI(KC_RIGHT), _______, _______, KC_MINS, KC_EQL, M(4), M(5), KC_BSLS},
+  {KC_LSFT, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,  TD(TD_LBRACKET), TD(TD_RBRACKET), _______, _______, KC_RSFT},
+  {RESET,   _______, _______, _______, _______, _______, _______, KC_ENT, KC_TRNS, _______, _______, _______}
+},
+
+/* Foward
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      | TOP  |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      | LWIN | WIN  | WIN  | RWIN |      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      | WIN  | WIN  |BOTTOM|      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | XXXX |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+  /* 12: top horizontal
+   * 13: bottom horizontal
+   *
+   * 8: corner left top
+   * 9: corner left bottom
+   * 10: corner right top
+   * 11: corner right bottom
+   *
+   * 6: vertical left
+   * 7: vertical right
+   */
+[3] = {
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, M(12), _______, _______},
+  {_______, _______, _______, _______, _______, _______, M(6), M(8), M(10), M(7), _______, _______},
+  {_______, _______, _______, _______, _______, _______, M(14), M(9), M(11), M(13), _______, _______},
+  {KC_TRNS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
 },
 
 };
@@ -111,6 +140,90 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         layer_off(2);
       }
       break;
+    case 3:
+      if(record->event.pressed){
+        layer_on(3);
+      } else {
+        layer_off(3);
+      }
+      break;
+    case 4: // Back delete 
+      if(record->event.pressed) {
+        return MACRO(D(LALT), D(RSHIFT), T(LEFT), T(BSPC), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 5: // Forward delete
+      if(record->event.pressed) {
+        return MACRO(D(LALT), D(RSHIFT), T(RIGHT), T(BSPC), END);
+      } else { 
+        clear_keyboard();
+      }
+    break;
+    case 6: // left window
+      if(record->event.pressed) {
+        return MACRO(D(LGUI), D(LCTL), T(LEFT), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 7: // right window
+      if(record->event.pressed) {
+        return MACRO(D(LGUI), D(LCTL), T(RIGHT), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 8: // left corner top
+      if(record->event.pressed) {
+        return MACRO(D(LCTL), D(LALT), T(UP), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 9: // left corner bottom
+      if(record->event.pressed) {
+        return MACRO(D(LCTL), D(LALT), T(DOWN), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 10: // right corner top
+      if(record->event.pressed) {
+        return MACRO(D(LALT), D(LGUI), T(UP), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 11: // right corner bottom
+      if(record->event.pressed) {
+        return MACRO(D(LALT), D(LGUI), T(DOWN), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 12: // half top 
+      if(record->event.pressed) {
+        return MACRO(D(LSFT), D(LALT), T(UP), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 13: // half bottom
+      if(record->event.pressed) {
+        return MACRO(D(LSFT), D(LALT), T(DOWN), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
+    case 14: // maximize window
+      if(record->event.pressed) {
+        return MACRO(D(LCTL), D(LALT), D(LGUI), T(M), END);
+      } else {
+        clear_keyboard();
+      }
+    break;
   }
   return MACRO_NONE;
 };
