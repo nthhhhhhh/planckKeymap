b540bee14ca01441100978d65e88a3182b71c677
joseph rangel <joseph@dialed.rip>
2022-06-17 11:18:36 -0500

    Update rules.mk
    remove muse don't use it, no audio, save space.

--- rules.mk
+++ rules.mk
@@ -1,8 +1,6 @@
-SRC              += muse.c
-
 LEADER_ENABLE     = yes
 TAP_DANCE_ENABLE  = yes
 COMMAND_ENABLE	  = no
+AUDIO_ENABLE      = no     # Audio output on port C6
 MOUSEKEY_ENABLE   = yes
-BOOTMAGIC_ENABLE  = full
 
